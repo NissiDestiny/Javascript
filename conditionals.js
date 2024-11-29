09cfe082c91e38565b8e17a46a8eb52f3cc2ed2e
NissiDestiny <nissidestiny@gmail.com>
2024-11-29 11:10:51 +0300

    Introduction to Javascript
    research
     and presentation

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+ var userName="Nissi"
+
+if (userName=="Nissi") {
+    console.log("Welcome Nissi");
+
+}
+
+
+console.log("outside if");
+
+let marks = 60;
+if(marks==10){
+    console.log('F9')
+
+}
+
+else if(marks==30){
+    console.log('P8')
+}
+
+else if(marks==60){
+    console.log('C5')
+
+}
+else{
+    console.log('I do not know')
+}
